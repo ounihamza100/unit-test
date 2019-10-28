245989ef80758d08f3efea930bb4a2f9b5c825cd
ounihamza100 <hamza.ellouni@gmail.com>
2019-10-28 15:31:49 +0100

    first commit

---
+++ unit-test/src/main/java/com/test/unit_test/App.java
@@ -0,0 +1,14 @@
+package com.test.unit_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
